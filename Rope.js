96c6ac070a66ced18f96b4d3ff10210878cb13d3
EarleyBird7 <72363693+EarleyBird7@users.noreply.github.com>
2021-01-19 15:56:35 -0800

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,27 @@
+class Rope{
+    constructor(bodyX,pointY){
+        var options={
+            bodyA:bodyX, 
+            pointB:pointY,
+            stiffness:0.2,
+            length:30
+        }
+    this.rope=Constraint.create(options)
+    this.pointB=pointY
+    World.add(world,this.rope)
+    }
+    fly(){
+        this.rope.bodyA=null
+    }
+    attach(bodyA){
+        this.rope.bodyA=bodyA
+    }
+display(){
+    if(this.rope.bodyA){        
+    var pointA=this.rope.bodyA.position
+    var pointB=this.pointB
+    strokeWeight(5);
+    line(pointA.x,pointA.y,pointB.x,pointB.y) 
+}
+}
+}
